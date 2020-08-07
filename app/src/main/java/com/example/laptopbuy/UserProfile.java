08bc86de6a94096b6147b38e2b3b8e2731d55c9c
Harwinder <happyhayer20@gmail.com>
2020-08-06 17:22:16 -0700

    Minor Change in User Profile

--- app/src/main/java/com/example/laptopbuy/UserProfile.java
+++ app/src/main/java/com/example/laptopbuy/UserProfile.java
@@ -2,13 +2,25 @@ package com.example.laptopbuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends AppCompatActivity {
-
+Button button=findViewById(R.id.buttonEditProfile);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UserProfile.this,EditUserProfile.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
