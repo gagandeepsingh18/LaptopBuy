70fcc76a835084624f978bccb5b99b029fab8ffa
Gagandeep Singh Saini <gagan_singh18@protonmail.com>
2020-09-08 16:41:27 -0400

    Started working on connections with Firestore

--- app/src/main/java/com/example/laptopbuy/LoginActivity.java
+++ app/src/main/java/com/example/laptopbuy/LoginActivity.java
@@ -7,9 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button Login,Register;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
